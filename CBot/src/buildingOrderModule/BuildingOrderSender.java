55dd86af6ff275f7fd2ce9ee8106e3f1988e8f49
P H <ph1387@t-online.de>
2017-04-28 17:57:04 +0200

    Improved interaction between the buildingOrder- and UnitControlModule

--- CBot/src/buildingOrderModule/BuildingOrderSender.java
+++ CBot/src/buildingOrderModule/BuildingOrderSender.java
@@ -1,26 +1,48 @@
 package buildingOrderModule;
 
+import bwapi.TechType;
 import bwapi.UnitType;
+import bwapi.UpgradeType;
 import core.CBot;
+import unitControlModule.UnitControlModule;
 
 /**
- * BuildingOrderSender.java --- Class for sending building orders to the UnitControlModule. 
+ * BuildingOrderSender.java --- Class for sending building orders to the
+ * UnitControlModule.
+ * 
  * @author P H - 26.04.2017
  *
  */
 public class BuildingOrderSender implements CommandSender {
-	
+
+	private UnitControlModule unitControlModule = CBot.getInstance().getUnitControlModule();
+
 	public BuildingOrderSender() {
-		
+
 	}
-	
+
 	// -------------------- Functions
 
 	public void buildUnit(UnitType unitType) {
-		CBot.getInstance().getUnitControlModule().addToTrainingQueue(unitType);
+		this.unitControlModule.addToTrainingQueue(unitType);
 	}
 
 	public void buildBuilding(UnitType unitType) {
-		CBot.getInstance().getUnitControlModule().addToTrainingQueue(unitType);
+		this.unitControlModule.addToTrainingQueue(unitType);
+	}
+
+	@Override
+	public void researchTech(TechType techType) {
+		this.unitControlModule.addToResearchQueue(techType);
+	}
+
+	@Override
+	public void buildAddon(UnitType addon) {
+		this.unitControlModule.addToAddonQueue(addon);
+	}
+
+	@Override
+	public void buildUpgrade(UpgradeType upgradeType) {
+		this.unitControlModule.addToUpgradeQueue(upgradeType);
 	}
 }
--- CBot/src/buildingOrderModule/CommandSender.java
+++ CBot/src/buildingOrderModule/CommandSender.java
@@ -1,10 +1,25 @@
 package buildingOrderModule;
 
+import bwapi.TechType;
 import bwapi.UnitType;
+import bwapi.UpgradeType;
 
+/**
+ * CommandSender.java --- Interface for communicating with the module that
+ * handles the construction of units and buildings as well as upgrades, etc.
+ * 
+ * @author P H - 26.04.2017
+ *
+ */
 public interface CommandSender {
 
 	public void buildUnit(UnitType unitType);
-	
+
 	public void buildBuilding(UnitType unitType);
+
+	public void researchTech(TechType techType);
+
+	public void buildAddon(UnitType addon);
+
+	public void buildUpgrade(UpgradeType upgradeType);
 }
