db880d4c60d9e42df79ea5aa398829c5f7aad72b
P H <ph1387@t-online.de>
2017-07-24 17:34:36 +0200

    Restrict the construction of refineries to the number of centers

--- CBot/src/buildingOrderModule/stateFactories/updater/ActionUpdaterSimulationQueueTerran.java
+++ CBot/src/buildingOrderModule/stateFactories/updater/ActionUpdaterSimulationQueueTerran.java
@@ -9,6 +9,7 @@ import buildingOrderModule.scoringDirector.ScoringDirectorTerran_Bio;
 import buildingOrderModule.simulator.ActionType;
 import buildingOrderModule.simulator.TypeWrapper;
 import buildingOrderModule.stateFactories.actions.AvailableActionsSimulationQueueTerran;
+import core.Core;
 import javaGOAP.GoapAction;
 
 // TODO: UML ADD
@@ -31,6 +32,10 @@ public class ActionUpdaterSimulationQueueTerran extends ActionUpdaterSimulationQ
 	protected HashSet<ActionType> generateAllAvailableActionTypes(BuildActionManager manager) {
 		HashSet<ActionType> availableActionTypes = new HashSet<>();
 		HashSet<GoapAction> availableActions = manager.getAvailableActions();
+		Integer playerCenterCount = manager.getCurrentGameInformation().getCurrentUnits()
+				.get(Core.getInstance().getPlayer().getRace().getCenter());
+		Integer playerRefineryCount = manager.getCurrentGameInformation().getCurrentUnits()
+				.get(Core.getInstance().getPlayer().getRace().getRefinery());
 
 		// Get all the Types plus their amount that are currently produced and
 		// inside the action Queue. Used to prevent i.e. building two centers.
@@ -54,8 +59,12 @@ public class ActionUpdaterSimulationQueueTerran extends ActionUpdaterSimulationQ
 					}
 					break;
 				case "UnitType_Terran_Refinery":
+					// Only allow the construction of refineries when the center
+					// count is larger than the refinery count.
 					if (!usedActionTypes.containsKey(TypeWrapper.UnitType_Terran_Refinery)
-							&& !forwardedActionTypes.containsKey(TypeWrapper.UnitType_Terran_Refinery)) {
+							&& !forwardedActionTypes.containsKey(TypeWrapper.UnitType_Terran_Refinery)
+							&& playerCenterCount != null && playerRefineryCount != null
+							&& playerCenterCount > playerRefineryCount) {
 						availableActionTypes.add(actionType);
 					}
 					break;
