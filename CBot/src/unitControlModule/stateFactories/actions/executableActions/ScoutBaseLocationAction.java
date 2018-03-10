faa4cee4241058214fb6195ce1ad4b0a57c9bca5
P H <ph1387@t-online.de>
2018-03-10 07:58:41 +0100

    Made scouting action use region access order for choosing BaseLocations

--- CBot/src/unitControlModule/stateFactories/actions/executableActions/ScoutBaseLocationAction.java
+++ CBot/src/unitControlModule/stateFactories/actions/executableActions/ScoutBaseLocationAction.java
@@ -1,10 +1,18 @@
 package unitControlModule.stateFactories.actions.executableActions;
 
 import java.util.HashMap;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.TreeSet;
 
 import bwapi.Position;
-import bwapi.TilePosition;
+import bwapiMath.graph.BreadthAccessGenerator;
+import bwapiMath.graph.IConnector;
+import bwapiMath.graph.IInstanceMapper;
+import bwapiMath.graph.RegionConnector;
+import bwapiMath.graph.RegionInstanceMapper;
 import bwta.BWTA;
 import bwta.BaseLocation;
 import bwta.Region;
@@ -21,6 +29,71 @@ import unitControlModule.unitWrappers.PlayerUnit;
  *
  */
 public class ScoutBaseLocationAction extends BaseAction {
+
+	// TODO: UML ADD
+	/**
+	 * DistantRegion.java --- A wrapper Class used for encapsulating a single
+	 * Region. The total distance needed to reach it as well as the accessible
+	 * other Regions are also stored in this Class.
+	 * 
+	 * @author P H - 10.03.2018
+	 *
+	 */
+	private class DistantRegion {
+
+		public double distance = -1.;
+		public Region region;
+		public HashSet<Region> accessibleRegions;
+
+		public DistantRegion(double distance, Region region, HashSet<Region> accessibleRegions) {
+			this.distance = distance;
+			this.region = region;
+			this.accessibleRegions = accessibleRegions;
+
+			// Prevent NullPointer Exceptions!
+			if (this.accessibleRegions == null) {
+				this.accessibleRegions = new HashSet<>();
+			}
+		}
+
+	}
+
+	// TODO: UML ADD
+	/**
+	 * DistantBaseLocation.java --- A wrapper Class used for encapsulating a
+	 * single BaseLocation. It stores the targeted BaseLocation as well as the
+	 * total distance towards it.
+	 * 
+	 * @author P H - 10.03.2018
+	 *
+	 */
+	private class DistantBaseLocation implements Comparable<DistantBaseLocation> {
+
+		public double distance = -1.;
+		public BaseLocation baseLocation;
+
+		public DistantBaseLocation(double distance, BaseLocation baseLocation) {
+			this.distance = distance;
+			this.baseLocation = baseLocation;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (obj instanceof DistantBaseLocation) {
+				return baseLocation.equals(((DistantBaseLocation) obj).baseLocation);
+			} else {
+				return super.equals(obj);
+			}
+		}
+
+		@Override
+		public int compareTo(DistantBaseLocation baseLocation) {
+			// BaseLocations with a shorter total distances are moved towards
+			// the front.
+			return Double.compare(this.distance, baseLocation.distance);
+		}
+
+	}
 
 	// TODO: UML CHANGE 60
 	// The time after a BaseLocation may be searched again.
@@ -61,7 +134,7 @@ public class ScoutBaseLocationAction extends BaseAction {
 		boolean executeMove = false;
 
 		if (this.targetPosition == null) {
-			this.targetPosition = this.findClosestReachableBasePosition();
+			this.targetPosition = this.findClosestReachableBasePosition(goapUnit);
 			executeMove = true;
 		}
 
@@ -74,75 +147,162 @@ public class ScoutBaseLocationAction extends BaseAction {
 		return success;
 	}
 
+	// TODO: UML VISIBILITY
 	/**
-	 * If no enemy buildings are are being known of the Bot has to search for
-	 * them.
+	 * Function for finding the closest BaseLocation, that must be visited by
+	 * the Unit in order to find enemy buildings.
+	 * 
+	 * @param goapUnit
+	 *            the executing Unit.
 	 * 
 	 * @return the closest, reachable base Position.
 	 */
-	protected Position findClosestReachableBasePosition() {
+	private Position findClosestReachableBasePosition(IGoapUnit goapUnit) {
+		TreeSet<DistantBaseLocation> distantBaseLocations = this.generateBaseLocationDistances(goapUnit);
 		Position closestPosition = null;
 
-		// The start locations must be preferred.
-		closestPosition = this.findCLosestReachableBaseLocation(BWTA.getStartLocations());
+		// The start locations must be preferred in order for them to be checked
+		// first.
+		closestPosition = this.findClosestReachableBasePosition(distantBaseLocations, false);
 
 		if (closestPosition == null) {
-			closestPosition = this.findCLosestReachableBaseLocation(BWTA.getBaseLocations());
+			closestPosition = this.findClosestReachableBasePosition(distantBaseLocations, true);
 		}
 
 		return closestPosition;
 	}
 
+	// TODO: UML PARAMS
+	// TODO: UML RENAME findCLosestReachableBaseLocation
 	/**
 	 * Function for finding the closest, reachable BaseLocation with a
 	 * timeStamp:
 	 * <p>
 	 * currentTime - timeStamp >= timePassed
 	 * 
-	 * @param baseLocations
+	 * @param distantBaseLocations
 	 *            the BaseLocations that are being checked.
+	 * @param includeNonStartingLocations
+	 *            set true if the function should include the non
+	 *            StartingLocations in the search. If set to false only the
+	 *            BaseLocations which can be started at are used.
 	 * 
 	 * @return the closest, reachable base Position with a matching timeStamp.
 	 */
-	private Position findCLosestReachableBaseLocation(List<BaseLocation> baseLocations) {
+	private Position findClosestReachableBasePosition(TreeSet<DistantBaseLocation> distantBaseLocations,
+			boolean includeNonStartingLocations) {
 		PlayerUnit playerUnit = (PlayerUnit) this.currentlyExecutingUnit;
 		HashMap<BaseLocation, Integer> baselocationsSearched = playerUnit.getInformationStorage()
 				.getBaselocationsSearched();
+		Iterator<DistantBaseLocation> treeIterator = distantBaseLocations.iterator();
 		Position closestReachableBasePosition = null;
-		int closestReachableBasePositionPathSize = -1;
 
-		for (BaseLocation location : baseLocations) {
-			Region baseRegion = ((BaseLocation) location).getRegion();
+		while (closestReachableBasePosition == null && treeIterator.hasNext()) {
+			BaseLocation baseLocation = treeIterator.next().baseLocation;
 
-			// TODO: Needed Change: Check for blocking minerals
-			if (baselocationsSearched.get(location) != null && playerUnit.getUnit().hasPath(baseRegion.getCenter())) {
-				boolean enoughTimePassed, isInitialBaseLocation, isBetterBaseLocation;
-				List<TilePosition> path = BWTA.getShortestPath(
-						((PlayerUnit) this.currentlyExecutingUnit).getUnit().getTilePosition(),
-						baseRegion.getCenter().toTilePosition());
+			if (baselocationsSearched.get(baseLocation) != null
+					&& playerUnit.getUnit().hasPath(baseLocation.getRegion().getCenter())) {
+				if (baseLocation.isStartLocation() || includeNonStartingLocations) {
+					boolean enoughTimePassed;
 
-				// Must be differentiated between because the scout will
-				// otherwise only stand at the starting BaseLocation until the
-				// time is reached (Then the actual scouting will begin).
-				if (Core.getInstance().getGame().elapsedTime() < BASELOCATIONS_TIME_PASSED) {
-					enoughTimePassed = baselocationsSearched.get(location).equals(0);
-				} else {
-					int timeDifference = Core.getInstance().getGame().elapsedTime()
-							- baselocationsSearched.get(location);
-					enoughTimePassed = timeDifference >= BASELOCATIONS_TIME_PASSED;
-				}
+					// Must be differentiated between because the scout will
+					// otherwise only stand at the starting BaseLocation until
+					// the time is reached (Then the actual scouting will
+					// begin).
+					if (Core.getInstance().getGame().elapsedTime() < BASELOCATIONS_TIME_PASSED) {
+						enoughTimePassed = baselocationsSearched.get(baseLocation).equals(0);
+					} else {
+						int timeDifference = Core.getInstance().getGame().elapsedTime()
+								- baselocationsSearched.get(baseLocation);
+						enoughTimePassed = timeDifference >= BASELOCATIONS_TIME_PASSED;
+					}
 
-				isInitialBaseLocation = closestReachableBasePosition == null && enoughTimePassed;
-				isBetterBaseLocation = !isInitialBaseLocation && enoughTimePassed
-						&& path.size() < closestReachableBasePositionPathSize;
-
-				if (isInitialBaseLocation || isBetterBaseLocation) {
-					closestReachableBasePosition = baseRegion.getCenter();
-					closestReachableBasePositionPathSize = path.size();
+					if (enoughTimePassed) {
+						closestReachableBasePosition = baseLocation.getRegion().getCenter();
+					}
 				}
 			}
 		}
+
 		return closestReachableBasePosition;
+	}
+
+	// TODO: UML ADD
+	/**
+	 * Function for generating the TreeSet with which the most suitable
+	 * BaseLocation to search is being calculated. This function utilizes a
+	 * Region access order to determine the order in which the different
+	 * distances must be added.
+	 * 
+	 * @param goapUnit
+	 *            the executing Unit whose current Region is used to determine
+	 *            the starting Region of the algorithm.
+	 * @return a TreeSet containing all searchable BaseLocations on the map
+	 *         wrapped inside DistantBaseLocation instances in order to compare
+	 *         their total distances to the provided Unit.
+	 */
+	private TreeSet<DistantBaseLocation> generateBaseLocationDistances(IGoapUnit goapUnit) {
+		HashMap<Region, HashSet<Region>> regionAccessOrder = this.generateRegionAccessOrder(goapUnit);
+		Region startRegion = BWTA.getRegion(((PlayerUnit) goapUnit).getUnit().getPosition());
+		TreeSet<DistantBaseLocation> baseLocationDistances = new TreeSet<>();
+
+		Queue<DistantRegion> regionsToCheck = new LinkedList<>();
+		regionsToCheck.add(new DistantRegion(0, startRegion, regionAccessOrder.get(startRegion)));
+
+		while (!regionsToCheck.isEmpty()) {
+			DistantRegion currentDistantRegion = regionsToCheck.poll();
+			Position currentCenter = currentDistantRegion.region.getCenter();
+
+			// Add all matching BaseLocations of the current Region to the
+			// TreeSet.
+			if (!currentDistantRegion.region.getBaseLocations().isEmpty()) {
+				for (BaseLocation baseLocation : currentDistantRegion.region.getBaseLocations()) {
+					double distanceFromRegionCenter = currentCenter.getDistance(baseLocation.getPosition());
+
+					baseLocationDistances.add(new DistantBaseLocation(
+							currentDistantRegion.distance + distanceFromRegionCenter, baseLocation));
+				}
+			}
+
+			for (Region region : currentDistantRegion.accessibleRegions) {
+				double distance = currentCenter.getDistance(region.getCenter());
+
+				regionsToCheck.add(new DistantRegion(distance, region, regionAccessOrder.get(region)));
+			}
+		}
+
+		return baseLocationDistances;
+	}
+
+	// TODO: UML ADD
+	/**
+	 * Function for generating the order in which the different Regions are
+	 * accessible beginning at one the Unit is currently in.
+	 * <ul>
+	 * <li>Key: Any Region of the currently played map.</li>
+	 * <li>Value: An HashSet of adjacent Regions that can be accessed by the key
+	 * Region.</li>
+	 * </ul>
+	 * 
+	 * @param goapUnit
+	 *            the executing Unit whose current Region is used.
+	 * @return a HashMap containing the breadth search equivalent of the access
+	 *         order of the different map Regions.
+	 */
+	private HashMap<Region, HashSet<Region>> generateRegionAccessOrder(IGoapUnit goapUnit) {
+		IConnector<Region> regionConnector = new RegionConnector();
+		IInstanceMapper<Region> regionInstanceMapper = new RegionInstanceMapper();
+		Region startRegion = BWTA.getRegion(((PlayerUnit) goapUnit).getUnit().getPosition());
+		HashMap<Region, HashSet<Region>> breadthAccessOrder = new HashMap<>();
+
+		try {
+			breadthAccessOrder = BreadthAccessGenerator.generateBreadthAccessOrder(regionInstanceMapper,
+					regionConnector, startRegion);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return breadthAccessOrder;
 	}
 
 	@Override
